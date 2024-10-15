db90153be16c6de549ac2dc65d133333191b01c3
Bilques-case <arhumbilques@gmail.com>
2024-10-15 10:47:55 +0500

    Task-03

---
+++ Calculator.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+cout <<"                               Hello This is a Calculator\n";
+
+int n1;
+int n2;
+cout <<"enter n1 => ";
+cin >>n1;
+cout <<"enter n2 => ";
+cin >>n2;
+
+cout <<"addition result\n";
+cout <<n1+n2;
+//-------------------above done---------------------------------------------
+cout <<"\nsubtraction result\n";
+cout <<n1-n2;
+cout <<"\nmultiplication result\n";
+cout <<n1*n2<<endl;
+cout <<"Happy Exit Thanks";
+
+
+return 0;
+}
